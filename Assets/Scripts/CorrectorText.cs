34657fe325cc31e0f292bf48d9cf8b100c3bd56b
Anekront <107873869+ASYarichuk@users.noreply.github.com>
2023-11-06 15:42:45 +0300

    Changes after 1 comments

--- Assets/Scripts/CorrectorText.cs
+++ Assets/Scripts/CorrectorText.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 
 public class CorrectorText : MonoBehaviour
 {
-    [SerializeField] Text _text1;
-    [SerializeField] Text _text2;
-    [SerializeField] Text _text3;
+    [SerializeField] private Text _textToBeReplaced;
+    [SerializeField] private Text _textToAddTextTo;
+    [SerializeField] private Text _textToBulkhead;
 
     private string _replacedText = "Замененный текст.";
     private string _addedText = "Добавленный текст.";
@@ -14,10 +14,10 @@ public class CorrectorText : MonoBehaviour
 
     private float _duration = 3f;
 
-    void Start()
+    private void Start()
     {
-        _text1.DOText(_replacedText, _duration);
-        _text2.DOText(_addedText, _duration).SetRelative();
-        _text3.DOText(_overdoneText, _duration, true, ScrambleMode.All);
+        _textToBeReplaced.DOText(_replacedText, _duration);
+        _textToAddTextTo.DOText(_addedText, _duration).SetRelative();
+        _textToBulkhead.DOText(_overdoneText, _duration, true, ScrambleMode.All);
     }
 }
